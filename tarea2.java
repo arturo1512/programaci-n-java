8d74754695c6345ebe35a51687c4740c0887c16f
arturo1512 <arturo-duende@hotmail.com>
2017-02-02 01:46:19 -0600

    Add files via upload

---
+++ tarea2.java
@@ -0,0 +1,21 @@
+class tarea2 {
+	public static void main(String[]args){
+int varentera =2147483647;
+byte varbyte=127;
+long varlongs=9223372036854775807l;
+short varshort=32767;
+char varchar=2;
+String varstring=null;
+String varho="hola java";
+System.out.println("valor tipo entero es igual a:"+varentera);
+System.out.println("Valor en tipo byte1 es igual:"+varbyte);
+System.out.println("valor de tipo long1 es:"+varlongs);
+System.out.println("valor de tipo short1 es:"+varshort);
+System.out.println("valor de tipo char1 es:"+varchar);
+System.out.println("la variable string vale:"+varstring);
+System.out.println("la variable string es:"+varho);
+	}
+
+
+
+}
---
+++ tarea2a.java
@@ -0,0 +1,24 @@
+class tarea2a {
+
+public static String var=null;
+public static byte varbyte=0;
+public static short varshort=0;
+public static int vari=0;
+public static long varlong=0l;
+public static double vardou=0.0d;
+public static char varchar= 'u' ;
+
+public static boolean varboo=false;
+
+	public static void main(String[]args){
+
+System.out.println("esto vale string: "+var);
+System.out.println("esto vale Byte: "+varbyte);
+System.out.println("esto vale Short: "+varshort);
+System.out.println("esto vale Int: "+vari);
+System.out.println("esto vale Long: "+varlong);
+System.out.println("esto vale Double: "+vardou);
+System.out.println("esto vale Char: "+varchar);
+System.out.println("esto vale Boolean: "+varboo);
+
+}}
