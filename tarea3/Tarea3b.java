443bd87a9847599f31f96e420ffde38b32a2119a
arturo1512 <arturo-duende@hotmail.com>
2017-03-08 17:29:24 -0600

    proyecto

---
+++ tarea3/Tarea3b.java
@@ -0,0 +1,22 @@
+public class Tarea3b {
+//este programa muestra los valores que se van sumando o restando usando operadores logicos
+
+	public static void main(String[]args){
+int result=0;
+int a=3;
+result+=a;
+System.out.println("el valor es"+result);
+
+result=a-1;
+System.out.println("el valor es "+result);
+result=a+1;
+System.out.println("el valor es"+result);
+result=a-1;
+System.out.println("el valor es"+result);
+result=a+7;
+System.out.println("El valor es"+result);
+result=a;
+System.out.println("EL valor es:"+result);
+
+	}
+}
